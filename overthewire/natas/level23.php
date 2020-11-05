4053ac8a80fc3a98fcde3116b61f69d234548fa8
jorsae <jorgen.sater@gmail.com>
2020-11-05 13:30:08 +1100

    Completed level24

---
+++ overthewire/natas/level23.php
@@ -0,0 +1,30 @@
+<?php
+        $request = '';
+        foreach($argv as $value){
+                $request = $value;
+        }
+
+        echo "REQUEST: " . $request . "\r\n";
+        if(strstr($request, "iloveyou")){
+                echo "strstr: true \r\n";
+        }
+        else{
+                echo "strstr: false \r\n";
+        }
+
+        if($request > 10){
+                echo "$request > 10: true \r\n";
+        }
+        else{
+                echo "$request > 10: false \r\n";
+        }
+
+
+        if(strstr($request,"iloveyou") && ($request > 10 )){
+        echo "<br>The credentials for the next level are:<br>";
+        echo "<pre>Username: natas24 Password: <censored></pre>";
+    }
+    else{
+        echo "<br>Wrong!<br>";
+    }
+?> 
