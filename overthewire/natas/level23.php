39523ccad8cda00361311941d81f4c642d24fb45
jorsae <jorgen.sater@gmail.com>
2020-11-05 13:34:01 +1100

    Fixed indentaton on level23.php

--- overthewire/natas/level23.php
+++ overthewire/natas/level23.php
@@ -1,26 +1,25 @@
 <?php
-        $request = '';
-        foreach($argv as $value){
-                $request = $value;
-        }
+    $request = '';
+    foreach($argv as $value){
+            $request = $value;
+    }
 
-        echo "REQUEST: " . $request . "\r\n";
-        if(strstr($request, "iloveyou")){
-                echo "strstr: true \r\n";
-        }
-        else{
-                echo "strstr: false \r\n";
-        }
+    echo "REQUEST: " . $request . "\r\n";
+    if(strstr($request, "iloveyou")){
+            echo "strstr: true \r\n";
+    }
+    else{
+            echo "strstr: false \r\n";
+    }
 
-        if($request > 10){
-                echo "$request > 10: true \r\n";
-        }
-        else{
-                echo "$request > 10: false \r\n";
-        }
+    if($request > 10){
+            echo "$request > 10: true \r\n";
+    }
+    else{
+            echo "$request > 10: false \r\n";
+    }
 
-
-        if(strstr($request,"iloveyou") && ($request > 10 )){
+    if(strstr($request,"iloveyou") && ($request > 10 )){
         echo "<br>The credentials for the next level are:<br>";
         echo "<pre>Username: natas24 Password: <censored></pre>";
     }
